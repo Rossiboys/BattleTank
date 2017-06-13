1738f4ef02f9bfdc9917def33df2ad456e5ba7c3
Jack Rossi <facerollingmole@gmail.com>
2017-06-13 01:58:00 +0100

    TankAimingComponent start

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -21,6 +21,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	class UTankAimingComponent* TankAimingComponent = nullptr;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
